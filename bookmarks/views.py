e5e18566b059f269aa5ed46475c20bd2235d846c
Pyunghyuk Yoo <yoophi@gmail.com>
2016-03-05 12:58:40 +0900

    user_page

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf8 -*-
-from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.http import HttpResponse, Http404
 from django.template import Context
 from django.template.loader import get_template
 
@@ -12,18 +13,20 @@ def main_page(request):
         'page_body': '여기에 북마크를 저장하고 공유할 수 있습니다.'
     })
     output = template.render(variables)
-    # output = '''
-    # <html>
-    # <head><title>%s</title></head>
-    # <body>
-    # <h1>%s</h1>
-    # <p>%s</p>
-    # </body>
-    # </html>
-    # ''' % (
-    #     '장고 | 북마크',
-    #     '장고북마크에 오신 것을 환영합니다',
-    #     '여기에 북마크를 저장하고 공유할 수 있습니다.'
-    # )
-    #
+    return HttpResponse(output)
+
+
+def user_page(request, username):
+    try:
+        user = User.objects.get(username=username)
+    except Exception:
+        raise Http404('사용자를 찾을 수 없습니다')
+
+    bookmarks = user.bookmark_set.all()
+    template = get_template('user_page.html')
+    variables = Context({
+        'username': username,
+        'bookmarks': bookmarks,
+    })
+    output = template.render(variables)
     return HttpResponse(output)
--- django_bookmarks/urls.py
+++ django_bookmarks/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from bookmarks.views import main_page
+from bookmarks.views import main_page, user_page
 
 urlpatterns = [
     url(r'^$', main_page),
+    url(r'user/(\w+)', user_page),
     url(r'^admin/', include(admin.site.urls)),
 ]
