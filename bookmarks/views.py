dda1ed3a67ab6eaacdbd6414d5adb8b78c901986
Pyunghyuk Yoo <yoophi@gmail.com>
2016-03-06 17:29:57 +0900

    login, logout 기능 추가

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,19 +1,17 @@
 # -*- coding: utf8 -*-
+from django.contrib.auth import logout
 from django.contrib.auth.models import User
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.shortcuts import render_to_response
 from django.template import Context
 from django.template.loader import get_template
 
 
 def main_page(request):
-    template = get_template('main_page.html')
-    variables = Context({
-        'head_title': '장고 | 북마크',
-        'page_title': '장고북마크에 오신 것을 환영합니다',
-        'page_body': '여기에 북마크를 저장하고 공유할 수 있습니다.'
-    })
-    output = template.render(variables)
-    return HttpResponse(output)
+    return render_to_response(
+        'main_page.html',
+        {'user': request.user}
+    )
 
 
 def user_page(request, username):
@@ -30,3 +28,8 @@ def user_page(request, username):
     })
     output = template.render(variables)
     return HttpResponse(output)
+
+
+def logout_page(request):
+    logout(request)
+    return HttpResponseRedirect('/')
--- django_bookmarks/urls.py
+++ django_bookmarks/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from bookmarks.views import main_page, user_page
+from bookmarks.views import main_page, user_page, logout_page
 
 urlpatterns = [
     url(r'^$', main_page),
     url(r'user/(\w+)', user_page),
+    url(r'login/$', 'django.contrib.auth.views.login'),
+    url(r'logout/$', logout_page),
     url(r'^admin/', include(admin.site.urls)),
 ]
