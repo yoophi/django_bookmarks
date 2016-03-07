47a702080fd0cea81fc22b3b617c5a8008d6fed4
Pyunghyuk Yoo <yoophi@gmail.com>
2016-03-07 14:05:32 +0900

    register new user

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from bookmarks.forms import RegistrationForm
 
 
 def main_page(request):
@@ -30,3 +31,21 @@ def user_page(request, username):
 def logout_page(request):
     logout(request)
     return HttpResponseRedirect('/')
+
+def register_page(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = User.objects.create_user(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password1'],
+                email=form.cleaned_data['email'],
+            )
+            return HttpResponseRedirect('/register/success/')
+    else:
+        form = RegistrationForm()
+
+    variables = RequestContext(request, {
+        'form': form
+    })
+    return render_to_response('registration/register.html', variables)
--- django_bookmarks/urls.py
+++ django_bookmarks/urls.py
@@ -2,8 +2,9 @@ import os
 
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
-from bookmarks.views import main_page, user_page, logout_page
+from bookmarks.views import main_page, user_page, logout_page, register_page
 
 site_media = os.path.join(os.path.dirname(__file__), 'site_media')
 urlpatterns = [
@@ -11,6 +12,11 @@ urlpatterns = [
     url(r'^user/(\w+)', user_page),
     url(r'^login/$', 'django.contrib.auth.views.login'),
     url(r'^logout/$', logout_page),
+    url(r'^register/$', register_page),
+    url(r'^register/success/$',
+        TemplateView.as_view(template_name='registration/register_success.html'),
+        name='register_success'
+        ),
     url(r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': site_media}),
     url(r'^admin/', include(admin.site.urls)),
 ]
