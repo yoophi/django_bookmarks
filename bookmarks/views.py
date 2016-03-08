97b2da0da8db8cf4d6cde76d4d33153dff7c13e3
Pyunghyuk Yoo <yoophi@gmail.com>
2016-03-09 00:49:00 +0900

    ajax 이용한 북마크 저장 기능

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -3,9 +3,10 @@ from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
-from django.http import Http404, HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
+
 from bookmarks.forms import RegistrationForm, BookmarkSaveForm, SearchForm
 from bookmarks.models import Link, Bookmark, Tag
 
@@ -56,13 +57,26 @@ def register_page(request):
 
 @login_required(login_url='/login/')
 def bookmark_save_page(request):
+    ajax = request.GET.has_key('ajax')
     if request.method == 'POST':
         form = BookmarkSaveForm(request.POST)
         if form.is_valid():
             bookmark = _bookmark_save(request, form)
-            return HttpResponseRedirect(
-                '/user/%s/' % request.user.username
-            )
+            if ajax:
+                variables = RequestContext(request, {
+                    'bookmarks': [bookmark],
+                    'show_edit': True,
+                    'show_tags': True,
+                })
+                return render_to_response('bookmark_list.html', variables)
+            else:
+                return HttpResponseRedirect(
+                    '/user/%s/' % request.user.username
+                )
+        else:
+            if ajax:
+                return HttpResponse('failure')
+
     elif request.GET.has_key('url'):
         url = request.GET['url']
         title = ''
@@ -79,7 +93,7 @@ def bookmark_save_page(request):
             )
         except ObjectDoesNotExist:
             pass
-        
+
         form = BookmarkSaveForm({
             'url': url,
             'title': title,
@@ -91,7 +105,10 @@ def bookmark_save_page(request):
     variables = RequestContext(request, {
         'form': form
     })
-    return render_to_response('bookmark_save.html', variables)
+    if ajax:
+        return render_to_response('bookmark_save_form.html', variables)
+    else:
+        return render_to_response('bookmark_save.html', variables)
 
 
 def tag_page(request, tag_name):
