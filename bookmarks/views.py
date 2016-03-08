02e3f26b15500b1e1fd7de393321d9d71966d315
Pyunghyuk Yoo <yoophi@gmail.com>
2016-03-08 11:52:22 +0900

    코드 정리

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -17,10 +17,6 @@ def main_page(request):
 
 
 def user_page(request, username):
-    # try:
-    #     user = User.objects.get(username=username)
-    # except Exception:
-    #     raise Http404('사용자를 찾을 수 없습니다')
     user = get_object_or_404(User, username=username)
     bookmarks = user.bookmark_set.order_by('-id')
 
