ce24422c71e8a7da529b01f471287158ea70042b
Pyunghyuk Yoo <yoophi@gmail.com>
2016-03-09 11:25:34 +0900

    tag autocomplete 기능 (스크립트 오류로 동작 안 함. 다른 라이브러리 이용해서 다시 구현할 것)

---
+++ django_bookmarks/site_media/tag_autocomplete.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    // TODO: 동작 안함. bgiframe 스크립트 에러. 다른 라이브러리로 구현할 것
+    //$('#id_tags').autocomplete(
+    //    '/ajax/tag/autocomplete/',
+    //    {multiple: true, multipleSeparator: ' '}
+    //);
+});
